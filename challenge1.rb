0d34796974aa7a64b11b285ce361bc06d82d93eb
George Sykes <georgesykes86@gmail.com>
2018-04-17 09:11:15 +0100

    Implementing all tests for challenge 1

--- challenge1.rb
+++ challenge1.rb
@@ -5,3 +5,16 @@ end
 def ascending_sort(array)
   array.sort
 end
+
+def sort_and_increment(array)
+  array = ascending_sort(array)
+  increment_by_one(array)
+end
+
+def sum_array(array)
+  "#{array.sum} (the sum)"
+end
+
+def double_sum(array)
+  "#{array.sum * 2} (twice the sum)"
+end
--- spec/challenge1_spec.rb
+++ spec/challenge1_spec.rb
@@ -8,4 +8,16 @@ describe 'Arrays transformer method' do
   it 'Sorts array into ascending order' do
     expect(ascending_sort([1, 3, 5, 4, 2])).to eq [1, 2, 3, 4, 5]
   end
+
+  it 'Generates a sorted array incremented by 1' do
+    expect(sort_and_increment([1, 3, 5, 4, 2])).to eq [2, 3, 4, 5, 6]
+  end
+
+  it 'Sums the total of all array elements' do
+    expect(sum_array([1, 2, 3, 4, 5])).to eq "15 (the sum)"
+  end
+
+  it 'Generates the sum multiplied by two' do
+    expect(double_sum([1, 2, 3, 4, 5])).to eq "30 (twice the sum)"
+  end
 end
