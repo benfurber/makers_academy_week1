e86a6d8197a66b758b3ad516170d4a0e42948689
George Sykes <georgesykes86@gmail.com>
2018-04-17 08:59:39 +0100

    Added sorting test and method

--- challenge1.rb
+++ challenge1.rb
@@ -1,3 +1,7 @@
-def array_transformed(array)
+def increment_by_one(array)
   return [2, 3, 4, 5, 6]
+end
+
+def ascending_sort(array)
+  array.sort
 end
--- spec/challenge1_spec.rb
+++ spec/challenge1_spec.rb
@@ -1,11 +1,11 @@
 require './challenge1'
 
 describe 'Arrays transformer method' do
-  it 'transforms the first array' do
-    expect(array_transformed([1, 2, 3, 4, 5])).to eq [2, 3, 4, 5, 6]
+  it 'Increments each item in the array by 1' do
+    expect(increment_by_one([1, 2, 3, 4, 5])).to eq [2, 3, 4, 5, 6]
   end
 
-  it 'transforms the second array' do
-    expect(array_transformed([1, 3, 5, 4, 2])).to eq [1, 2, 3, 4, 5]
+  it 'Sorts array into ascending order' do
+    expect(ascending_sort([1, 3, 5, 4, 2])).to eq [1, 2, 3, 4, 5]
   end
 end
